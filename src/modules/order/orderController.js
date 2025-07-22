cbe4803d865c459410fa2f8553bd788bbe6642c6
mostafa <mostafa.hamdy.3377@gmail.com>
2025-07-23 02:29:23 +0300

    update order

--- src/modules/order/orderController.js
+++ src/modules/order/orderController.js
@@ -3,6 +3,9 @@ import { orderModel } from "../../../database/models/orderModel.js";
 import { AppError } from "../../../utils/appError.js";
 import { catchError } from "../../middleware/catchError.js";
 import { productModel } from "../../../database/models/productModel.js";
+
+import Stripe from "stripe";
+
 const createCashOrder = catchError(async (req, res, next) => {
   const cart = await cartModel.findById(req.params.id);
   if (!cart) return next(new AppError("card not found", 404));
@@ -13,24 +16,73 @@ const createCashOrder = catchError(async (req, res, next) => {
     user: req.user._id,
     orderItems: req.body.cartItems,
     totalOrderPrice,
-    shippingAdress: req.body.shippingAdress,
+    shippingAddress: req.body.shippingAddress,
   });
-
-  const options = cart.cartItems.map((prod) => {
-    return [
-      {
-        updateOne: {
-          filter: { _id: prod.product },
-          update: { $inc: { sold: prod.quantity, quantity: -prod.quantity } },
+  // increment sold & decrement product quantity
+  const options = cart.cartItems.map((item) => {
+    return {
+      updateOne: {
+        filter: { _id: item.product },
+        update: {
+          $inc: { quantity: -item.quantity, sold: item.quantity },
         },
       },
-    ];
+    };
   });
-
   await productModel.bulkWrite(options);
+
+  // clear cart items
+  await cartModel.findByIdAndDelete(req.params.id);
 
   await order.save();
   res.json({ message: "success", order });
 });
 
-export { createCashOrder };
+const getSpecificOrder = catchError(async (req, res, next) => {
+  const order = await orderModel
+    .findOne({ user: req.user._id })
+    .populate("cartItems.product");
+  res.status(200).json({ message: "success", order });
+});
+const getAllOrders = catchError(async (req, res, next) => {
+  const orders = await orderModel.find({}).populate("orderItems.product");
+  res.status(200).json({ message: "success", orders });
+});
+
+const createCheackOutSession = catchError(async (req, res, next) => {
+  const cart = await cartModel.findById(req.params.id);
+  if (!cart) return next(new AppError("card not found", 404));
+  const totalOrderPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalPrice;
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "egp",
+          product_data: {
+            name: req.user.name,
+          },
+          unit_amount: totalOrderPrice * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+    metadata: req.body.shippingAddress,
+    client_reference_id: req.params.id, // cart id
+    customer_email: req.user.email,
+  });
+
+  res.json({ message: "success", session });
+});
+
+export {
+  createCashOrder,
+  getSpecificOrder,
+  getAllOrders,
+  createCheackOutSession,
+};
--- src/modules/order/orderRouts.js
+++ src/modules/order/orderRouts.js
@@ -2,13 +2,31 @@ import express from "express";
 import { createOrderVal } from "./orderValidation.js";
 import { allowedTo, protectedRoutes } from "../auth/authController.js";
 import { validation } from "../../middleware/validation.js";
-import { createCashOrder } from "./orderController.js";
+import {
+  createCashOrder,
+  createCheackOutSession,
+  getAllOrders,
+  getSpecificOrder,
+} from "./orderController.js";
 const orderRouter = express.Router();
-orderRouter.post(
-  "/:id",
-  protectedRoutes,
-  allowedTo("user"),
-  validation(createOrderVal),
-  createCashOrder
-);
+orderRouter
+  .route("/:id")
+  .post(
+    protectedRoutes,
+    allowedTo("user"),
+    validation(createOrderVal),
+    createCashOrder
+  );
+orderRouter
+  .route("/checkOut/:id")
+  .post(
+    protectedRoutes,
+    allowedTo("user"),
+    validation(createOrderVal),
+    createCheackOutSession
+  );
+orderRouter.get("/all", protectedRoutes, allowedTo("admin"), getAllOrders);
+orderRouter
+  .route("/")
+  .get(protectedRoutes, allowedTo("user"), getSpecificOrder);
 export { orderRouter };
